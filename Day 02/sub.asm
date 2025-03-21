7937c96c5a34862859cc40a205d50b61576bb4c7
NishaAnjalin <139215902+NishaAnjalin@users.noreply.github.com>
2025-03-21 10:14:15 +0530

    Day 02 March 21

---
+++ Day 02/sub.asm
@@ -0,0 +1,24 @@
+.model small
+.stack 100h
+.data
+
+n1 db 8
+n2 db 5
+
+.code
+
+start:
+
+mov ax, @data
+mov ds, ax
+
+mov dl, n1
+sub dl, n2
+add dl, 48
+
+mov ah, 02h
+int 21h
+
+mov ax, 4c00h
+int 21h
+end start
